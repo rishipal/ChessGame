8c2328d8b5385b7e550d17ddd6b1306d75ab7059
Rishipal <rishipalbhatia@gmail.com>
2019-12-31 10:00:12 -0800

    Revert "Remove jupiter tests and added org.junit.Test"
    This reverts commit 93851e68620f25ed4a887a48eceba8a9815b3c95.

--- src/com/rishi/tests/PawnTests.java
+++ src/com/rishi/tests/PawnTests.java
@@ -1,7 +1,27 @@
 package com.rishi.tests;
 
 import com.rishi.chess.ChessBoard;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class PawnTests {
+    ChessBoard board;
+
+    @BeforeEach
+    void setUp() {
+        board = new ChessBoard();
+    }
+
+    @Test
+    public void testPawnPiecesMarkedOccupied() {
+        for(int j = 0; j < board.SIZE_BOARD; j++) {
+            assert(this.board.getChessBoard()[1][j].occupied);
+            assert(this.board.getChessBoard()[7][j].occupied);
+        }
+    }
+
+    @Test
+    public void testPawnMoves() {
+
+    }
 }
